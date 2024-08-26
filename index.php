ebd86e15b840493af7608d9ed144db02d67c67c1
Jay Varner <jayvarner@gmail.com>
2024-08-26 16:46:40 -0400

    Fix horz overflow - for real

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
-    <link rel="icon" type="image/png" href="/themes/bigpicture//images/moon-icon.png" />
+    <link rel="icon" type="image/png" href="/themes/bigpicture/images/moon-icon.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Apollo 15 Learning Hub</title>
-    <script type="module" crossorigin src="/themes/bigpicture//assets/index-CtscTy15.js"></script>
-    <link rel="stylesheet" crossorigin href="/themes/bigpicture//assets/index-DKCJeqIq.css">
+    <script type="module" crossorigin src="/themes/bigpicture/assets/index-DFBUTg9D.js"></script>
+    <link rel="stylesheet" crossorigin href="/themes/bigpicture/assets/index-DKCJeqIq.css">
   </head>
   <body>
     <div id="root"></div>
